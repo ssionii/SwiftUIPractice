7b48181ba1a282a9739f21756b1d5f4e36e1f221
양시연 <ssionii@mobmail.shinhan.com>
2021-02-22 15:22:57 +0900

    practice creating and combining views

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by  60117280 on 2021/02/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
